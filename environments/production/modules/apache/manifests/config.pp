91e746927f2d5a3ad7d7b8e457402ca516fa20fb
simialfapro <simip@DESKTOP-1MC56G7>
2024-09-16 10:16:29 +0200

    fixes

--- environments/production/modules/apache/manifests/config.pp
+++ environments/production/modules/apache/manifests/config.pp
@@ -1,10 +1,4 @@
 class apache::config {
-  file { '/etc/httpd/conf.d/vhost.conf':
-    ensure  => file,
-    content => template('apache/apache.conf.erb'),  #
-    require => Package['httpd'],
-  }
-
   file { '/var/www/html/index.html':
     ensure  => file,
     source  => 'puppet:///modules/apache/index.html', 
---
+++ environments/production/modules/apache/manifests/vhosts.pp
@@ -0,0 +1,11 @@
+class apache::vhost {
+  $server_admin  = 'admin@example.com'
+  $document_root = '/var/www/html'
+  $server_name   = 'www.example.com'
+  $server_alias  = 'example.com'
+
+  file { '/etc/apache2/sites-available/000-default.conf':
+    ensure  => file,
+    content => template('apache/vhost.conf.erb'),
+  }
+}
