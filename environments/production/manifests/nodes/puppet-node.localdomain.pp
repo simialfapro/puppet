a5967903654b18dda835a44f7dea553e8537b5aa
simialfapro <simip@DESKTOP-1MC56G7>
2024-09-16 10:27:06 +0200

    fixes

--- environments/production/manifests/nodes/puppet-node.localdomain.pp
+++ environments/production/manifests/nodes/puppet-node.localdomain.pp
@@ -2,10 +2,5 @@
 include apache
 
 node 'puppet-node.localdomain' {
-  class { 'apache::vhosts':
-    server_admin  => lookup('apache::server_admin'),
-    document_root => lookup('apache::document_root'),
-    server_name   => lookup('apache::server_name'),
-    server_alias  => lookup('apache::server_alias'),
-  }
+
 }
