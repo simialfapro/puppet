670e14ff644302ccb592a4eb7f9da96b1aae0664
simialfapro <simip@DESKTOP-1MC56G7>
2024-09-16 10:28:35 +0200

    fixes

--- environments/production/modules/apache/manifests/config.pp
+++ environments/production/modules/apache/manifests/config.pp
@@ -2,6 +2,6 @@ class apache::config {
   file { '/var/www/html/index.html':
     ensure  => file,
     source  => 'puppet:///modules/apache/index.html', 
-    require => Package['httpd'],
+    require => Package['apache2'],
   }
 }
