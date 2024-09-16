a54ae8af0d7aca0c6c121931c7d5f4cb0940cf83
simialfapro <simip@DESKTOP-1MC56G7>
2024-09-16 13:35:43 +0200

    fixes

--- environments/production/modules/apache/manifests/vhosts.pp
+++ environments/production/modules/apache/manifests/vhosts.pp
@@ -4,6 +4,10 @@ class apache::vhosts (
   String $server_name   = 'www.example.com',
   String $server_alias  = 'example.com',
 ) {
+
+  $vhosts = hiera_hash('apache::vhosts', {})
+  create_resources('apache::vhost', $vhosts)
+  
   file { '/etc/apache2/sites-available':
     ensure => directory,
     mode   => '0755',
