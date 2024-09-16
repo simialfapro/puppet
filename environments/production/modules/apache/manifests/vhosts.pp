4499b2a030d068957d65aada88edda23fc85ad43
simialfapro <simip@DESKTOP-1MC56G7>
2024-09-16 10:22:55 +0200

    fixes

--- environments/production/modules/apache/manifests/vhosts.pp
+++ environments/production/modules/apache/manifests/vhosts.pp
@@ -1,8 +1,9 @@
-class apache::vhosts {
-  $server_admin  = 'admin@example.com'
-  $document_root = '/var/www/html'
-  $server_name   = 'www.example.com'
-  $server_alias  = 'example.com'
+class apache::vhosts (
+  String $server_admin  = 'admin@example.com',
+  String $document_root = '/var/www/html',
+  String $server_name   = 'www.example.com',
+  String $server_alias  = 'example.com',
+) {
 
   file { '/etc/apache2/sites-available/000-default.conf':
     ensure  => file,
