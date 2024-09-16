fe2a7ef173e0f31ca72eaf74495194aa7e41a1fe
simialfapro <simip@DESKTOP-1MC56G7>
2024-09-16 15:13:59 +0200

    notify

--- environments/production/modules/apache/manifests/vhosts.pp
+++ environments/production/modules/apache/manifests/vhosts.pp
@@ -1,6 +1,6 @@
 class apache::vhosts {
   # Lese die VHosts-Daten aus Hiera. Falls keine vorhanden sind, wird ein leeres Hash zurückgegeben.
-  $vhosts = hiera_hash('apache::vhosts', {})
+  $vhosts = lookup('apache::vhosts', {})
   notify {"vhosts: $vhosts": }
   # Verwende `create_resources`, um dynamisch VHosts aus den Hiera-Daten zu erstellen
   create_resources('apache::vhost', $vhosts)
