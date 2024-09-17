bd4aa73127a35f7b740d3a146d94d910e317889b
simialfapro <simip@DESKTOP-1MC56G7>
2024-09-17 10:55:11 +0200

    changing init

--- environments/production/modules/apache/manifests/init.pp
+++ environments/production/modules/apache/manifests/init.pp
@@ -5,13 +5,20 @@ class apache {
   $vhosts = lookup('apache::vhosts', { 'default_value' => [] })
   notify { "VHosts data: ${vhosts}": }
   # Iteration über die VHosts und Erstellen jedes VHosts
-  $vhosts['vhosts'].each |$name, $vhost| {
+  $vhosts.each |$vhost| {
     apache::vhost { $vhost['servername']:
       servername => $vhost['servername'],
       docroot    => $vhost['docroot'],
       port       => $vhost['port'],
       ssl        => $vhost['ssl'],
     }
+    /*
+    class { 'apache::vhost':
+      servername => $vhost['servername'],
+      docroot    => $vhost['docroot'],
+      port       => $vhost['port'],
+      ssl        => $vhost['ssl'],
+    }*/
   }
 }
 
