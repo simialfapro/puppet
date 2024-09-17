8c1e0be7d144c20685b1b6e3ba995e79c07045c6
simialfapro <simip@DESKTOP-1MC56G7>
2024-09-17 07:40:32 +0200

    declaring class

--- environments/production/modules/apache/manifests/init.pp
+++ environments/production/modules/apache/manifests/init.pp
@@ -3,15 +3,18 @@ class apache {
   include apache::config
   include apache::service
   # Abrufen der VHost-Daten aus Hiera
+  
   $vhosts = lookup('apache::vhosts', { 'default_value' => {} })
   notify { "VHosts data: ${vhosts}": }
   # Iteration über die VHosts und Erstellen jedes VHosts
   $vhosts['vhosts'].each |$name, $vhost| {
-    apache::vhost { $vhost['servername']:
-      servername => $vhost['servername'],
-      docroot    => $vhost['docroot'],
-      port       => $vhost['port'],
-      ssl        => $vhost['ssl'],
+    class { 'apache': 
+      vhosts => {
+        servername =>  $vhost['servername'],
+        docroot    => $vhost['docroot'],
+        port       => $vhost['port'],
+        ssl        => $vhost['ssl'],
+      }
     }
   }
 }
