84146b97286cced159e3f0e2da3ccdce09ebecc7
simialfapro <simip@DESKTOP-1MC56G7>
2024-09-17 10:40:36 +0200

    fixing init

--- environments/production/modules/apache/manifests/init.pp
+++ environments/production/modules/apache/manifests/init.pp
@@ -2,18 +2,6 @@ class apache {
   include apache::install
   include apache::config
   include apache::service
-  # Abrufen der VHost-Daten aus Hiera
-  
-  $vhosts = lookup('apache::vhosts', { 'default_value' => [] })
-  notify { "VHosts data: ${vhosts}": }
-  # Iteration über die VHosts und Erstellen jedes VHosts
-  $vhosts['vhosts'].each |$name, $vhost| {
-    apache::vhost { $vhost['servername']:
-      servername => $vhost['servername'],
-      docroot    => $vhost['docroot'],
-      port       => $vhost['port'],
-      ssl        => $vhost['ssl'],
-    }
-  }
+  include apache::vhost
 }
 
--- environments/production/modules/apache/manifests/vhost.pp
+++ environments/production/modules/apache/manifests/vhost.pp
@@ -4,6 +4,17 @@ class apache::vhost (
   Optional[String] $port = '80',
   Optional[Boolean] $ssl = false,
 ) {
+  $vhosts = lookup('apache::vhosts', { 'default_value' => [] })
+  notify { "VHosts data: ${vhosts}": }
+  # Iteration über die VHosts und Erstellen jedes VHosts
+  $vhosts['vhosts'].each |$name, $vhost| {
+    apache::vhost { $vhost['servername']:
+      servername => $vhost['servername'],
+      docroot    => $vhost['docroot'],
+      port       => $vhost['port'],
+      ssl        => $vhost['ssl'],
+    }
+  }
   # Erstelle die Konfigurationsdatei für den VHost
   file { "/etc/apache2/sites-enabled/${servername}.conf":
     ensure  => file,
