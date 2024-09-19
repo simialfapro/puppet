83875d49e49ccdbca2d1bba91fa8221981b1b338
simialfapro <simip@DESKTOP-1MC56G7>
2024-09-19 08:39:44 +0200

    fixing commands

--- environments/production/modules/openssl/manifests/config.pp
+++ environments/production/modules/openssl/manifests/config.pp
@@ -3,7 +3,7 @@ class openssl::config (
   String $state          = 'Zürich',
   String $locality       = 'Zürich',
   String $organization   = '3dbambuu',
- String $common_name    = $trusted['certname'],
+  String $common_name    = $trusted['certname'],
   String $cert_path      = '/etc/ssl/certs',
   String $key_path       = '/etc/ssl/private',
   Integer $days          = 365,             
@@ -25,17 +25,17 @@ class openssl::config (
     content => template('openssl/openssl.cnf.erb'),
   }
   exec { "generate_private_key_${common_name}":
-    command => "openssl genrsa -out ${key_file} 2048",
+    command => "/usr/bin/openssl genrsa -out ${key_file} 2048",
     creates => $key_file,
     require => File[$key_path],
   }
   exec { "generate_csr_${common_name}":
-    command => "openssl req -new -key ${key_file} -out ${csr_file} -config /tmp/openssl.cnf",
+    command => "/usr/bin/openssl req -new -key ${key_file} -out ${csr_file} -config /tmp/openssl.cnf",
     creates => $csr_file,
     require => Exec["generate_private_key_${common_name}"],
   }
   exec { "generate_self_signed_cert_${common_name}":
-    command => "openssl x509 -req -days ${days} -in ${csr_file} -signkey ${key_file} -out ${cert_file}",
+    command => "/usr/bin/openssl x509 -req -days ${days} -in ${csr_file} -signkey ${key_file} -out ${cert_file}",
     creates => $cert_file,
     require => Exec["generate_csr_${common_name}"],
   }
